0a1bdad02ea3f0aba24ff2ff7f6f335fb0f31bb3
ram <ccvy.ram@gmail.com>
2022-08-19 14:22:52 +0530

    Arithmetic Demo

--- Arithmetic operations Demo.py
+++ Arithmetic operations Demo.py
@@ -1,7 +1,7 @@
 print("Enter two values:")
 x = int(input())
 y = int(input())
-print("Additon is:", x + y)
+print("Addition is:", x + y)
 print("Subtraction is:", x - y)
 print("Multiplication is:", x * y)
 print("Division is:", x / y)
